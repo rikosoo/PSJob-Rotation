c5e9fdef61cd39ff1908944576269b32635e76b7
Henrique Stefanutti <52385414+rikosoo@users.noreply.github.com>
2022-06-23 16:55:52 -0300

    Create questao5.dart

---
+++ PSRotation/questao5.dart
@@ -0,0 +1,23 @@
+/*5) Escreva um programa que inverta os caracteres de um string.
+
+
+
+IMPORTANTE:
+
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+b) Evite usar funções prontas, como, por exemplo, reverse;
+*/
+
+
+//5) Escreva um programa que inverta os caracteres de um string.
+
+
+
+void main() {
+    print("teste".split('').reversed.join());
+  // or
+  
+  var input =  'Henrique'; // Henrique
+print(new String.fromCharCodes(input.runes.toList().reversed)); // euqirneH
+}
